3206a23fc7b6f209e00b86f9fc4bd44f893e362e
varmaabhay <40569951+varmaabhay@users.noreply.github.com>
2019-01-16 15:44:55 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -23,8 +23,8 @@ async def download_file(url, dest):
             with open(dest, 'wb') as f: f.write(data)
 
 async def setup_learner():
-    await download_file(model_file_url, path_img/'models'/f'{model_file_name}.pth')
-    data_bunch = ImageDataBunch.single_from_classes(path_img, classes, tfms=get_transforms(), size=150).normalize(imagenet_stats)
+    await download_file(model_file_url, path/'models'/f'{model_file_name}.pth')
+    data_bunch = ImageDataBunch.single_from_classes(path, classes, tfms=get_transforms(), size=150).normalize(imagenet_stats)
     learn = create_cnn(data_bunch, models.resnet34, pretrained=False)
     learn.load(model_file_name)
     return learn
