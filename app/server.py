5ea625cc5993e8b62be96718be2c63ad622cdb2d
varmaabhay <40569951+varmaabhay@users.noreply.github.com>
2019-01-16 15:34:11 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -7,10 +7,8 @@ from io import BytesIO
 from fastai.vision import *
 import base64
 
-model_file_url = 'https://github.com/pankymathur/Fine-Grained-Clothing-Classification/blob/master/data/cloth_categories/models/stage-1_sz-150.pth?raw=true'
-model_file_name = 'model'
-classes = ['Blouse', 'Blazer', 'Button-Down', 'Bomber', 'Anorak', 'Tee', 'Tank', 'Top', 'Sweater', 'Flannel', 'Hoodie', 'Cardigan', 'Jacket', 'Henley', 'Poncho', 'Jersey', 'Turtleneck', 'Parka', 'Peacoat', 'Halter', 'Skirt', 'Shorts', 'Jeans', 'Joggers', 'Sweatpants', 'Jeggings', 'Cutoffs', 'Sweatshorts', 'Leggings', 'Culottes', 'Chinos', 'Trunks', 'Sarong', 'Gauchos', 'Jodhpurs', 'Capris', 'Dress', 'Romper', 'Coat', 'Kimono', 'Jumpsuit', 'Robe', 'Caftan', 'Kaftan', 'Coverup', 'Onesie']
-
+model_file_url = 'https://drive.google.com/uc?export=download&id=18ZR6IwQ3xDH_tEWeNs8XPxhpA0ew1KQX'
+model_file_name = 'deployment_stage-1.3'
 path = Path(__file__).parent
 
 app = Starlette()
@@ -25,8 +23,8 @@ async def download_file(url, dest):
             with open(dest, 'wb') as f: f.write(data)
 
 async def setup_learner():
-    await download_file(model_file_url, path/'models'/f'{model_file_name}.pth')
-    data_bunch = ImageDataBunch.single_from_classes(path, classes, tfms=get_transforms(), size=150).normalize(imagenet_stats)
+    await download_file(model_file_url, path_img/'models'/f'{model_file_name}.pth')
+    data_bunch = ImageDataBunch.single_from_classes(path_img, classes, tfms=get_transforms(), size=150).normalize(imagenet_stats)
     learn = create_cnn(data_bunch, models.resnet34, pretrained=False)
     learn.load(model_file_name)
     return learn
@@ -36,29 +34,17 @@ tasks = [asyncio.ensure_future(setup_learner())]
 learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
 
-PREDICTION_FILE_SRC = path/'static'/'predictions.txt'
+@app.route('/')
+def index(request):
+    html = path/'view'/'index.html'
+    return HTMLResponse(html.open().read())
 
-@app.route("/upload", methods=["POST"])
-async def upload(request):
+@app.route('/analyze', methods=['POST'])
+async def analyze(request):
     data = await request.form()
-    img_bytes = await (data["img"].read())
-    bytes = base64.b64decode(img_bytes)
-    return predict_from_bytes(bytes)
-
-def predict_from_bytes(bytes):
-    img = open_image(BytesIO(bytes))
-    _,_,losses = learn.predict(img)
-    predictions = sorted(zip(classes, map(float, losses)), key=lambda p: p[1], reverse=True)
-    result_html1 = path/'static'/'result1.html'
-    result_html2 = path/'static'/'result2.html'
-    
-    result_html = str(result_html1.open().read() +str(predictions[0:3]) + result_html2.open().read())
-    return HTMLResponse(result_html)
-
-@app.route("/")
-def form(request):
-    index_html = path/'static'/'index.html'
-    return HTMLResponse(index_html.open().read())
+    img_bytes = await (data['file'].read())
+    img = open_image(BytesIO(img_bytes))
+    return JSONResponse({'result':learn.predict(img)})
 
 if __name__ == "__main__":
     if "serve" in sys.argv: uvicorn.run(app, host="0.0.0.0", port=8080)
