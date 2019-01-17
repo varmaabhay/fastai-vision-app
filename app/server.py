b29458dd310ba7d430eadc0a069aa6a6ff42f9f7
varmaabhay <40569951+varmaabhay@users.noreply.github.com>
2019-01-17 09:20:33 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -38,7 +38,6 @@ async def setup_learner():
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
 learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
-return future.result()
 loop.close()
 
 @app.route('/')
